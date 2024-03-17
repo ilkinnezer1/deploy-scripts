d4ec7c6b63dd6a278fda825d4482c26b97228e70
Farhad <62416812+Khobolov@users.noreply.github.com>
2024-03-17 18:19:49 +0400

    Add certs volume to nginx

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -8,5 +8,6 @@ sudo docker run --detach \
   --network="$NETWORK" \
   --volume "$WEB_PATH":/usr/share/nginx/html \
   --volume "$CONFIG_PATH":/etc/nginx/conf.d \
+  --volume ${CERT_DIR}:/etc/nginx/certs \
   --restart unless-stopped \
   nginx:"$VERSION"
--- nginx/env.example.sh
+++ nginx/env.example.sh
@@ -1,5 +1,6 @@
 CONTAINER_NAME="nginx"
 WEB_PATH="/var/docker/nginx/data"
 CONFIG_PATH="/var/docker/nginx/config"
+CERT_DIR="/var/docker/nginx/certs"
 NETWORK="main"
 VERSION="1.23.4-alpine3.17"
