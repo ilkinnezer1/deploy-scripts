eaa5f863b9981e323878f18bd3751c19656ab0e4
Farhad <62416812+Khobolov@users.noreply.github.com>
2024-03-24 03:21:32 +0400

    Add publish ports to RabbitMQ bash script

--- rabbitmq/deploy.sh
+++ rabbitmq/deploy.sh
@@ -3,15 +3,18 @@ echo "load env.."
 . $1.sh
 
 bash ../remove.sh "$CONTAINER_NAME"
+
 sudo docker run --detach \
+  --restart unless-stopped \
+  --network="$NETWORK" \
+  --publish 15672:15672 \
+  --publish 5672:5672 \
   --name "$CONTAINER_NAME" \
   --hostname "$HOST_NAME" \
   --env RABBITMQ_DEFAULT_USER="$RABBITMQ_DEFAULT_USER" \
   --env RABBITMQ_DEFAULT_PASS="$RABBITMQ_DEFAULT_PASS" \
   --env RABBITMQ_NODENAME="$HOST_NAME" \
-  --network="$NETWORK" \
   --volume "$DATA_PATH":/var/lib/rabbitmq \
   --volume "$LOG_PATH":/var/log/rabbitmq \
-  --restart unless-stopped \
   rabbitmq:"$VERSION"
 chmod -R a+rwx "$SERVICE_PATH"  
