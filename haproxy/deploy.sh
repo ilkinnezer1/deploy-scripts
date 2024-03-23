b51183b9f597fa9b6aa3c03fca8f96008cb8ab6d
Farhad <62416812+Khobolov@users.noreply.github.com>
2024-03-24 02:58:21 +0400

    Add bash script for HAProxy

---
+++ haproxy/deploy.sh
@@ -0,0 +1,13 @@
+echo "load env.."
+. env.sh
+
+# Remove the container if it exists
+bash ../remove.sh "$CONTAINER_NAME"
+
+sudo docker run -d \
+   --name "$CONTAINER_NAME" \
+   --net "$NETWORK" \
+   -p 80:80 \
+   -p 8404:8404 \
+   -v $CONFIG_PATH:/usr/local/etc/haproxy:ro \
+   haproxy:2.9.6-alpine3.19
---
+++ haproxy/env.example.sh
@@ -0,0 +1,3 @@
+NETWORK="main"
+CONTAINER_NAME="ha-proxy"
+CONFIG_PATH="/var/docker/haproxy/config"
