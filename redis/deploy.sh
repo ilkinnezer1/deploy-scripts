24dca029491e580dbe918f784d0fd3bec532e812
Farhad <62416812+Khobolov@users.noreply.github.com>
2024-03-26 03:46:30 +0400

    Add "publish" flag to Redis bash script

--- redis/deploy.sh
+++ redis/deploy.sh
@@ -3,11 +3,13 @@ echo "load env.."
 . $1.sh
 
 bash ../remove.sh "$CONTAINER_NAME"
+
 sudo docker run --detach \
-  --name "$CONTAINER_NAME" \
-  --env REDIS_PASSWORD="$REDIS_PASSWORD" \
-  --network="$NETWORK" \
-  --volume "$DATA_PATH":/data \
   --restart unless-stopped \
+  --network="$NETWORK" \
+  --publish 6379:6379 \
+  --name "$CONTAINER_NAME" \
+  --volume "$DATA_PATH":/data \
+  --env REDIS_PASSWORD="$REDIS_PASSWORD" \
   redis:"$VERSION" \
 redis-server --appendonly yes  --requirepass "$REDIS_PASSWORD"
