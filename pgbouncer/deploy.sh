b4a0860f9810942f94ff9623d8ee3a62ca37b626
Xobolov <farhad.khobolov@gmail.com>
2024-04-04 12:44:17 +0400

    Add new ENVs to PgBouncer
    - PGBOUNCER_MAX_CLIENT_CONN
    - PGBOUNCER_DEFAULT_POOL_SIZE

--- pgbouncer/deploy.sh
+++ pgbouncer/deploy.sh
@@ -22,6 +22,8 @@ sudo docker run --detach \
   --env=PGBOUNCER_CLIENT_TLS_SSLMODE="$PGBOUNCER_CLIENT_TLS_SSLMODE" \
   --env=PGBOUNCER_CLIENT_TLS_CERT_FILE=/opt/bitnami/pgbouncer/certs/pgbouncer.crt \
   --env=PGBOUNCER_CLIENT_TLS_KEY_FILE=/opt/bitnami/pgbouncer/certs/pgbouncer.key \
+  --env=PGBOUNCER_MAX_CLIENT_CONN=5000 \
+  --env=PGBOUNCER_DEFAULT_POOL_SIZE=100 \
   --volume "$CERTS_PATH":/opt/bitnami/pgbouncer/certs \
   --restart unless-stopped \
   bitnami/pgbouncer:"$VERSION"
