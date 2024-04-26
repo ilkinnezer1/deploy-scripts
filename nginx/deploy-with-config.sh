cd1a50f275d62e7cab6ab14a2af546e9d30eb663
Xobolov <farhad.khobolov@gmail.com>
2024-04-26 11:38:43 +0400

    Add publish port to nginx script for https conn

--- nginx/deploy-with-config.sh
+++ nginx/deploy-with-config.sh
@@ -3,11 +3,13 @@ echo "load env.."
 bash ../remove.sh "$CONTAINER_NAME"
 sudo docker run --detach \
   --name "$CONTAINER_NAME" \
+  --publish 443:443 \  
   --publish 80:80 \
   --network="$NETWORK" \
   --volume "$LOG_PATH":/var/log/nginx \
   --volume "$WEB_PATH":/usr/share/nginx/html \
   --volume "$CONFIG_PATH":/etc/nginx/conf.d \
+  --volume "$CERTS_PATH":/etc/nginx/ssl \
   --volume "$ROOT_PATH"/nginx.conf:/etc/nginx/nginx.conf:ro \
   --volume "$IP_DB"/country.dat:/etc/nginx/country.dat:ro \
   --volume "$IP_DB"/city.dat:/etc/nginx/city.dat:ro \
