7a1311f359a3c3cf3429c7728109841b0eb08c5c
Xobolov <farhad.khobolov@gmail.com>
2024-09-13 12:37:44 +0400

    fix: remove `hostname` from rabbitmq

--- rabbitmq/deploy.sh
+++ rabbitmq/deploy.sh
@@ -10,11 +10,10 @@ sudo docker run --detach --restart unless-stopped \
   --publish 15672:15672 \
   --publish 5672:5672 \
   --name "$CONTAINER_NAME" \
-  --hostname "$HOST_NAME" \
   --env RABBITMQ_DEFAULT_USER="$RABBITMQ_DEFAULT_USER" \
   --env RABBITMQ_DEFAULT_PASS="$RABBITMQ_DEFAULT_PASS" \
   --env RABBITMQ_NODENAME="$HOST_NAME" \
-  --volume "$DATA_PATH":/var/lib/rabbitmq \
+  --volume "$DATA_PATH":/var/lib/rabbitmq/mnesia \
   --volume "$LOG_PATH":/var/log/rabbitmq \
   rabbitmq:"$VERSION"
 chmod -R a+rwx "$SERVICE_PATH"  
