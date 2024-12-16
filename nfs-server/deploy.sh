895e546e72232f95601a65a8564ea8ccce4e2811
Xobolov <farhad.khobolov@gmail.com>
2024-12-16 18:00:15 +0400

    feature: create 'script` for `nfs-server`

---
+++ nfs-server/deploy.sh
@@ -0,0 +1,10 @@
+echo "load env.."
+. env.sh
+bash ../remove.sh $CONTAINER_NAME
+docker run -d --privileged --restart unless-stopped \
+  --name $CONTAINER_NAME \
+  --network $NETWORK \
+  -p $EXPOSE_PORT:2049 \
+  -e SHARED_DIRECTORY=/nfsshare \
+  -v $DATA_PATH:/nfsshare \
+  itsthenetwork/nfs-server-alpine:$VERSION
---
+++ nfs-server/env.example.sh
@@ -0,0 +1,6 @@
+NETWORK="main"
+EXPOSE_PORT=2049
+CONTAINER_NAME="nfs-server"
+VERSION="latest"
+
+DATA_PATH="/var/docker/nfsshare"
