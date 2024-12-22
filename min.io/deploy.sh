52ca1d2b6d731b54212bd2a3dbf10d57c5615cc6
Xobolov <farhad.khobolov@gmail.com>
2024-12-22 19:03:20 +0400

    feature: add `deploy` script for `min.io`

---
+++ min.io/deploy.sh
@@ -0,0 +1,7 @@
+docker run --detach --restart unless-stopped \
+  --network main \
+  --name minio \
+  -p 9000:9000 \
+  -p 9001:9001 \
+  -v /var/docker/minio/data:/data \
+jumpserver/minio:latest server /data --console-address ":9001"
