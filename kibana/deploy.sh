8ac053b93adbed0c89c4ca0a52d8fb969f2b9a08
Xobolov <farhad.khobolov@gmail.com>
2025-01-22 23:12:14 +0400

    refactor: `kibana` deploy script

--- kibana/deploy.sh
+++ kibana/deploy.sh
@@ -1,8 +1,8 @@
 docker run --detach --restart unless-stopped \
-    --name kibana \
-    --network main \
-    --publish 5601:5601 \
-    --env ELASTICSEARCH_HOSTS='["http://elasticsearch:9200"]' \
-    --env ELASTICSEARCH_USERNAME="kibana" \
-    --env ELASTICSEARCH_PASSWORD="-JvvAbT*Hy1UV8Pz9iJ0" \
-docker.elastic.co/kibana/kibana:8.17.1
+    --name $CONTAINER_NAME \
+    --network $NETWORK \
+    --publish $PORT:5601 \
+    --env ELASTICSEARCH_HOSTS=$ELASTICSEARCH_HOSTS \
+    --env ELASTICSEARCH_USERNAME=$ELASTICSEARCH_USERNAME \
+    --env ELASTICSEARCH_PASSWORD=$ELASTICSEARCH_PASSWORD \
+docker.elastic.co/kibana/kibana:$VERSION
