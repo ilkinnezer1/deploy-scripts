5259eb976b5339a037ed1ce7c060650dac577bd5
Xobolov <farhad.khobolov@gmail.com>
2025-01-23 12:28:23 +0400

    feat: add `remove.sh` usage to `kibana` script

--- kibana/deploy.sh
+++ kibana/deploy.sh
@@ -1,8 +1,12 @@
-docker run --detach --restart unless-stopped \
+echo "load env.."
+. env.sh
+bash ../remove.sh $CONTAINER_NAME
+
+sudo docker run --detach --restart unless-stopped \
     --name $CONTAINER_NAME \
     --network $NETWORK \
     --publish $PORT:5601 \
     --env ELASTICSEARCH_HOSTS=$ELASTICSEARCH_HOSTS \
     --env ELASTICSEARCH_USERNAME=$ELASTICSEARCH_USERNAME \
     --env ELASTICSEARCH_PASSWORD=$ELASTICSEARCH_PASSWORD \
-docker.elastic.co/kibana/kibana:$VERSION
+    docker.elastic.co/kibana/kibana:$VERSION
