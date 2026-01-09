def57828347e5a47bff274ef76e4c68687b343b3
Khobolov <62416812+Khobolov@users.noreply.github.com>
2026-01-09 00:59:50 +0000

    feat: add port environment to `redis` script

--- redis/deploy.sh
+++ redis/deploy.sh
@@ -7,7 +7,7 @@ bash ../remove.sh "$CONTAINER_NAME"
 sudo docker run --detach \
   --restart unless-stopped \
   --network="$NETWORK" \
-  --publish 6379:6379 \
+  --publish $PORT:6379 \
   --name "$CONTAINER_NAME" \
   --volume "$DATA_PATH":/data \
   --env REDIS_PASSWORD="$REDIS_PASSWORD" \
--- redis/env.example.sh
+++ redis/env.example.sh
@@ -2,4 +2,5 @@ CONTAINER_NAME="redis"
 REDIS_PASSWORD="superpassword"
 DATA_PATH="/var/docker/$CONTAINER_NAME/data"
 NETWORK="main"
+PORT=6379
 VERSION="7.0-alpine3.16"
